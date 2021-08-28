b0aa4dad63ef77b816f584f96d5f8274be88b7a9
Sergio Barbosa <88118456+sergioabarbosas@users.noreply.github.com>
2021-08-27 21:56:50 -0500

    Update T2_mainFourier_Filtros.py
    Mejora Taller N2 Banco de filtros _ filtro con máscara std local

--- T2_mainFourier_Filtros.py
+++ T2_mainFourier_Filtros.py
@@ -2,148 +2,138 @@ import cv2
 import numpy as np
 import os
 import sys
-import math
-
 
 """ =================================================================================
---------TALLER_2 : Banco de filtros utilizando la trasnformada de Fourier----------
---------------------Creado por: Sergio B - Christian F.----------------------------
+--------TALLER_2 : Banco de filtros orientación - Fourier----------
+--------------------Presentado por: Sergio B - Christian Forero.----------------------------
 -----------------Alumnos: Maestría en Inteligencia Artificial---------------------
 ====================================================================================="""
 
+""" Fuente: Código FFT based filtering - Autor: Prof. Julian Quiroga
+    python fft_filtering.py <path_to_image> <image_name>
+"""
+
+## Falta definir la clase, el constructor y el metodo de set
 
 class thetaFilter:
 
     def __init__(self, img):
-	
         self.image = img
 
-    # Se crea el método para recibir parámetros 𝜃 y Δ𝜃 que definen la respuesta del filtro.
     def  set_theta(self,tetha,D_theta):
-        self.tetha=math.radians(tetha)
-        self.D_theta=math.radians(D_theta)
-        self.theta2= self.tetha-math.radians(180)
+        self.tetha=tetha
+        self.D_theta=D_theta
+        return self.tetha, self.D_theta
 
-	# Método para implementar un filtrado FFT componentes frecuencia orientada
-    def filtering(self):
+    def filtering (self):
 
-        # Convertimos la imagen BGR a escala gris
         image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         image_gray_fft = np.fft.fft2(image_gray)
         image_gray_fft_shift = np.fft.fftshift(image_gray_fft)
+
+        # fft visualization
         image_gray_fft_mag = np.absolute(image_gray_fft_shift)
+        image_fft_view = np.log(image_gray_fft_mag + 1)
+        image_fft_view = image_fft_view / np.max(image_fft_view)
+
+        # pre-computations
         num_rows, num_cols = (image_gray.shape[0], image_gray.shape[1])
         enum_rows = np.linspace(0, num_rows - 1, num_rows)
         enum_cols = np.linspace(0, num_cols - 1, num_cols)
         col_iter, row_iter = np.meshgrid(enum_cols, enum_rows)
-        # Calculamos el centro de la imagen
-        half_size= num_rows / 2 - 1
-        half_size2 = num_cols / 2 - 1
+        half_size = num_rows / 2  # here we assume num_rows = num_columns
 
-        # band pass filter mask
-        band_pass_mask = np.zeros_like(image_gray)
+        # orientation-based filter mask
+        orientation_mask = np.zeros_like(image_gray)
 
-        # Se crea el if para los theta 0°, 45°, 135°, 180°
-        if (self.tetha>= math.radians(0) and self.tetha<= math.radians(45)) or (self.tetha>= math.radians(135) and self.tetha<= math.radians(180)) :
+        # Verficamos la oreintación del píxel
+        orientation = 180 * np.arctan2(row_iter - half_size, col_iter - half_size) / np.pi + 180
+        idx_orientation_larger = orientation < self.tetha + self.D_theta
+        idx_orientation_lesser = orientation > self.tetha - self.D_theta
+        idx_orientation_1 = np.bitwise_and(idx_orientation_larger,
+                                           idx_orientation_lesser)  # Unimos las orientaciones mediante una operación lógica (AND)
+        idx_orientation_larger = orientation < self.tetha + 180 + self.D_theta
+        idx_orientation_lesser = orientation > self.tetha + 180 - self.D_theta
+        idx_orientation_2 = np.bitwise_and(idx_orientation_larger, idx_orientation_lesser)
+        idx_orientation = np.bitwise_or(idx_orientation_1, idx_orientation_2)
+        orientation_mask[idx_orientation] = 1
+        orientation_mask[int(half_size), int(half_size)] = 1
 
-            idx_low = np.arctan2((col_iter - half_size+15),(row_iter - half_size)) > self.theta2
-            idx_high = np.arctan2((col_iter - half_size+15),(row_iter - half_size)) < self.tetha
-            idx_low2 = np.arctan2((col_iter - half_size2-15), (row_iter - half_size)) > self.theta2
-            idx_high3 = np.arctan2((col_iter - half_size2-15), (row_iter - half_size)) < self.tetha
-            idx_bp = np.bitwise_and(idx_low, idx_high)
-            idx_bp2 = np.bitwise_and(idx_low2, idx_high3)
-            idx_bp3 = np.bitwise_xor(idx_bp2, idx_bp)
+        # filtering via FFT
+        mask = orientation_mask  # can also use high or band pass mask
+        fft_filtered = image_gray_fft_shift * mask
+        image_filtered = np.fft.ifft2(np.fft.fftshift(fft_filtered))
+        image_filtered = np.absolute(image_filtered)
+        image_filtered /= np.max(image_filtered)
 
-            idx_low2 = np.arctan2((col_iter - half_size + 15), (row_iter - half_size)) > self.theta2+self.D_theta
-            idx_high2 = np.arctan2((col_iter - half_size + 15), (row_iter - half_size)) < self.tetha+self.D_theta
-            idx_low22 = np.arctan2((col_iter - half_size2 - 15), (row_iter - half_size)) > self.theta2+self.D_theta
-            idx_high32 = np.arctan2((col_iter - half_size2 - 15), (row_iter - half_size)) < self.tetha+self.D_theta
-            idx_bp2 = np.bitwise_and(idx_low2, idx_high2)
-            idx_bp22 = np.bitwise_and(idx_low22, idx_high32)
-            idx_bp32 = np.bitwise_xor(idx_bp22, idx_bp2)
+        return image_filtered, mask
 
-            idx_bp33 = np.bitwise_or(idx_bp32, idx_bp3)
+def std_N (image, N, thresh):
 
-            band_pass_mask[idx_bp33] = 1
+    # Calculamos (M1 - M2) vecindarios de la imagen
+    image_x = cv2.blur(image, (N, N))
+        # Elevamos cada pixel de la imagen al cuadrado
+    image_x_2 = cv2.blur(np.power(image, 2 ), (N, N))
+        # Calculo desviación estándar
+    image_std = np.sqrt(image_x_2 - np.power(image_x, 2))
+        #  Normalizamos la imagen
+    image_std /= np.max(image_std)
+    mask_std = image_std > thresh
 
-            mask = band_pass_mask   # can also use high or band pass mask
-            fft_filtered = image_gray_fft_shift * mask
-            image_filtered = np.fft.ifft2(np.fft.fftshift(fft_filtered))
-            image_filtered = np.absolute(image_filtered)
-            image_filtered /= np.max(image_filtered)
-            # cv2.imshow("Filter frequency response", 255 * mask)
-            # cv2.imshow("Filtered image", image_filtered)
-            # cv2.waitKey(0)
-            return image_filtered, 255 * mask
-
-        # Se crea esta condición para permitir el filtrado de frecuencias.
-        if (self.tetha>= math.radians(46) and self.tetha<= math.radians(134))  :
-
-            idx_low = np.arctan2((col_iter - half_size),(row_iter - half_size+15)) > self.theta2
-            idx_high = np.arctan2((col_iter - half_size),(row_iter - half_size+15)) < self.tetha
-            idx_low2 = np.arctan2((col_iter - half_size2), (row_iter - half_size-15)) > self.theta2
-            idx_high3 = np.arctan2((col_iter - half_size2), (row_iter - half_size-15)) < self.tetha
-            idx_bp = np.bitwise_and(idx_low, idx_high)
-            idx_bp2 = np.bitwise_and(idx_low2, idx_high3)
-            idx_bp3 = np.bitwise_xor(idx_bp2, idx_bp)
-
-            idx_low2 = np.arctan2((col_iter - half_size ), (row_iter - half_size+ 15)) > self.theta2+self.D_theta
-            idx_high2 = np.arctan2((col_iter - half_size ), (row_iter - half_size+ 15)) < self.tetha+self.D_theta
-            idx_low22 = np.arctan2((col_iter - half_size2 ), (row_iter - half_size- 15)) > self.theta2+self.D_theta
-            idx_high32 = np.arctan2((col_iter - half_size2 ), (row_iter - half_size- 15)) < self.tetha+self.D_theta
-            idx_bp2 = np.bitwise_and(idx_low2, idx_high2)
-            idx_bp22 = np.bitwise_and(idx_low22, idx_high32)
-            idx_bp32 = np.bitwise_xor(idx_bp22, idx_bp2)
-
-            idx_bp33 = np.bitwise_or(idx_bp32, idx_bp3)
-
-            band_pass_mask[idx_bp33] = 1
-
-            mask = band_pass_mask   # can also use high or band pass mask
-            fft_filtered = image_gray_fft_shift * mask
-            image_filtered = np.fft.ifft2(np.fft.fftshift(fft_filtered))
-            image_filtered = np.absolute(image_filtered)
-            image_filtered /= np.max(image_filtered)
-            return image_filtered, 255 * mask
-            # cv2.imshow("Filter frequency response", 255 * mask)
-            # cv2.imshow("Filtered image", image_filtered)
-            # cv2.waitKey(0)
-
+    return mask_std
 
 if __name__ == '__main__':
 
-    # Se ingresa el path de la imagen (huellas)
-    img=cv2.imread("C:\\Users\\FAMILIAR\\Desktop\\sbarbosa\\Universidad_Javeriana\\Proc_imagenes_video\\Images\\huellas\\01\\01_1.tif")
+    img = cv2.imread("01_1.tif")
+    filtrada0 = thetaFilter(img)
+    filtrada0.set_theta(0, 20)
+    img_0 = filtrada0.filtering()[0]
 
-    # Se crea el objeto para el filtro de 0°
-    filtrada1=thetaFilter(img)
-    filtrada1.set_theta(0, 5)
-    imagen1 = filtrada1.filtering()[0]
+    img_45 = cv2.imread("01_1.tif")
+    filtrada45 = thetaFilter(img_45)
+    filtrada45.set_theta(45, 20)
+    img_45 = filtrada45.filtering()[0]
+    mask_45= filtrada45.filtering()[1]
 
-    # Se crea el objeto para el filtro de 45°
-    filtrada2 = thetaFilter(img)
-    filtrada2.set_theta(45, 5)
-    imagen2 = filtrada2.filtering()[0]
+    img_90 = cv2.imread("01_1.tif")
+    filtrada90 = thetaFilter(img_90)
+    filtrada90.set_theta(90, 20)
+    img_90 = filtrada90.filtering()[0]
 
-    # Se crea el objeto para el filtro de 90°
-    filtrada3 = thetaFilter(img)
-    filtrada3.set_theta(90, 5)
-    imagen3 = filtrada3.filtering()[0]
+    img_135 = cv2.imread("01_1.tif")
+    filtrada135 = thetaFilter(img_135)
+    filtrada135.set_theta(135, 20)
+    img_135 = filtrada135.filtering()[0]
 
-    # Se crea el objeto para el filtro de 135°
-    filtrada4 = thetaFilter(img)
-    filtrada4.set_theta(135, 5)
-    imagen4 = filtrada4.filtering()[0]
+    ## Método desviación estandar
+    # Calculamos el promedio con una ventana de 11
+    N = 11
+    # Valor Thresh
+    thresh = 0.5
+    # Definimos máscara con la desviación estándar local
+    mask_std_0 = std_N(img_0, N, thresh)
+    mask_std_45 = std_N(img_45, N, thresh)
+    mask_std_90 = std_N(img_90, N, thresh)
+    mask_std_135 = std_N(img_135, N, thresh)
 
-    # Se pormedian
-    final_image = cv2.addWeighted(imagen1, 0.5, imagen2, 0.5, 0.0)
-    final_image2 = cv2.addWeighted(imagen3, 0.5, imagen4, 0.5, 0.0)
-    final_image3 = cv2.addWeighted(final_image, 0.5, imagen2, 0.5, 0.0)
+    # Creamos la imagen sintetizada
+    resulting_image = mask_std_0.astype(np.float) * img_0 + mask_std_45.astype(
+        np.float) * img_45 + mask_std_90.astype(np.float) * img_90 + mask_std_135.astype(np.float) * img_135
 
-    # Visualizar las imagenes con filtros
-    cv2.imshow("Final image ",final_image)
-    cv2.imshow(" 0 grados ", imagen1)
-    cv2.imshow(" 45 grados ", imagen2)
-    cv2.imshow(" 90 grados ", imagen3)
-    cv2.imshow(" 135 grados ", imagen4)
+    # Hacemos el promedio de las máscaras de la stdr local
+    sum_mask = mask_std_0.astype(np.float) + mask_std_45.astype(
+        np.float) + mask_std_90.astype(np.float) + mask_std_135.astype(np.float)
+
+    cv2.imshow("Original image", img)
+    #cv2.imshow("Filter frequency response", 255 * mask_45)
+
+
+    #cv2.imshow("45° Std image", 255 * (mask_std_45.astype(np.uint8)))
+    cv2.imshow("0 Orientation Filtered image", img_0)
+    cv2.imshow("45 Orientation Filtered image", img_45)
+    cv2.imshow("90 Orientation Filtered image", img_90)
+    cv2.imshow("135 Orientation Filtered image", img_135)
+
+    cv2.imshow("Resulting image", resulting_image)
+    cv2.imshow("Sum mask", sum_mask/4)
     cv2.waitKey(0)
